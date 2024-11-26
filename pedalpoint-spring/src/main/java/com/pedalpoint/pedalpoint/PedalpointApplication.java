aa515ce6ed66f83cd08efa468761cfd94df74197
Manoj Parasuram Sadanala <manojparasuram.sadhanala@gmail.com>
2024-11-26 23:24:54 +0530

    Initial commit

---
+++ pedalpoint-spring/src/main/java/com/pedalpoint/pedalpoint/PedalpointApplication.java
@@ -0,0 +1,13 @@
+package com.pedalpoint.pedalpoint;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PedalpointApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PedalpointApplication.class, args);
+	}
+
+}
---
+++ pedalpoint-spring/src/test/java/com/pedalpoint/pedalpoint/PedalpointApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pedalpoint.pedalpoint;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PedalpointApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
